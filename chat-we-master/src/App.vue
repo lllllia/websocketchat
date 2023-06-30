c9d8b47d244f05f5f815335ec9c6eac369bd92e5
liujia <2458906876@qq.com>
2023-06-30 23:50:03 +0800

    chatroom

---
+++ chat-we-master/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { useUser } from '@/stores/user'
+import { watch } from 'vue';
+import { Socket } from 'socket.io-client';
+
+const state = useUser();
+
+// 当用户信息发生变化更新数据
+watch(() => state.user, () => {
+  window.sessionStorage.setItem("user", JSON.stringify(state.user));
+});
+
+// 当前在线人数
+(window as any).socket.on("online", (res:any) => {
+  state.online = res.count;
+  if(res.user.uuid === state.user.uuid) state.user.online = res.user.online;
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
